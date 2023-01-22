40e70b47b4f290b2aa6de4fe30abf84dcd04fe44
hayriye uylukcu <huylukcu@gmail.comgit config --global user.email huylukcu@gmail.comgit config --global user.name hayriye>
2023-01-22 16:59:07 -0600

    commit

---
+++ src/main/java/mypackage/Class1.java
@@ -0,0 +1,30 @@
+package mypackage;
+public class Class1 {
+    public static void main(String[]args){
+
+                double d = 2.3;
+
+                if (d < 0) {
+
+                    System.out.println("Negative");
+                }
+
+                if (d >= 0) {
+
+                    System.out.println("non negative");
+                }
+
+                int i = 15;
+
+
+                if( i%5==0) {
+                    System.out.println("divisible by 5");
+                }
+                if(i%5 !=0){
+
+                    System.out.println("not divisible by 5");
+
+                }
+            }
+
+        }
---
+++ src/main/java/mypackage/Class2.java
@@ -0,0 +1,11 @@
+package mypackage;
+
+public class Class2 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        String str = "studying=g github";
+        System.out.println(str);
+
+
+    }
+}
