ce184751e3dddab2e0a071e19eb9899a90b6c277
hayriye uylukcu <huylukcu@gmail.comgit config --global user.email huylukcu@gmail.comgit config --global user.name hayriye>
2023-01-23 12:04:44 -0600

    commit

--- src/main/java/mypackage/Class2.java
+++ src/main/java/mypackage/Class2.java
@@ -3,7 +3,7 @@ package mypackage;
 public class Class2 {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        String str = "studying=g github";
+        String str = "studying github";
         System.out.println(str);
 
     }
