e63419b3aeef70b2652ac3a4df1dd9ccb483ade1
hayriye uylukcu <huylukcu@gmail.comgit config --global user.email huylukcu@gmail.comgit config --global user.name hayriye>
2023-01-29 22:08:31 -0600

    commit

--- src/main/java/mypackage/Class3.java
+++ src/main/java/mypackage/Class3.java
@@ -3,6 +3,7 @@ package mypackage;
 public class Class3 {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("hello");
+        System.out.println("deneme pull request");
+
     }
 }
